3346692a546a944bd58f67edafbd75b709448b30
methnii <methniranasingha.gmail.com>
2025-10-06 00:12:12 +0530

    feat(model): add IngredientsItem entity with id field
    Created IngredientsItem entity with autogenerated id as the primary key.
    Added Lombok annotations for constructors, getters, and setters to reduce boilerplate.
    Prepared the class for future expansion to include ingredient details.

---
+++ src/main/java/com/foodo/model/IngredientsItem.java
@@ -0,0 +1,34 @@
+package com.foodo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IngredientsItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id ;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
